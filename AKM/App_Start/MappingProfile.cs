b59750133148e555bc69676a1a64e725e59d727b
Alon Koren <alonkoren312@gmail.com>
2019-09-10 20:39:15 +0300

    CustomerDto

---
+++ AKM/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AKM.Dtos;
+using AKM.Models;
+using AutoMapper;
+
+namespace AKM.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
