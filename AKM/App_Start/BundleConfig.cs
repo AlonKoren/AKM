d354555887d1640041f7f16b8915253f337973c6
Alon Koren <alonkoren312@gmail.com>
2019-09-10 19:01:32 +0300

    add validations

--- AKM/App_Start/BundleConfig.cs
+++ AKM/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace AKM
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css"));
         }
     }
 }
--- AKM/Controllers/CustomersController.cs
+++ AKM/Controllers/CustomersController.cs
@@ -1,18 +1,14 @@
 ﻿using AKM.Models;
 using AKM.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using AKM.ViewModels;
 
 namespace AKM.Controllers
 {
     public class CustomersController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
         public CustomersController()
         {
             _context = new ApplicationDbContext();
@@ -28,14 +24,25 @@ namespace AKM.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             return View("CustomerForm" ,viewModel);
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if(customer.Id==0)
                 _context.Customers.Add(customer);
             else
