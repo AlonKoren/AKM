e9efe6fe54a28b09fda3f40de3c04627f409f7cd
Alon Koren <alonkoren312@gmail.com>
2019-09-11 14:58:44 +0300

    register with facebook

--- AKM/App_Start/FilterConfig.cs
+++ AKM/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace AKM
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
--- AKM/Controllers/AccountController.cs
+++ AKM/Controllers/AccountController.cs
@@ -158,7 +158,8 @@ namespace AKM.Controllers
                 {
                     UserName = model.Email,
                     Email = model.Email,
-                    DrivingLicense = model.DrivingLicense
+                    DrivingLicense = model.DrivingLicense,
+                    Phone = model.Phone
                 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
@@ -376,7 +377,12 @@ namespace AKM.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    DrivingLicense = model.DrivingLicense
+                };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
